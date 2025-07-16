4055287ebc7a1ca50a1e2a6b637e68bc0af10371
Samridh Shukla <smshukla2020@gmail.com>
2025-07-16 15:11:16 +0530

    spislave.v

--- SPI_COMMUNICATION/SPI_COMMUNICATION.srcs/sources_1/new/spislave.v
+++ SPI_COMMUNICATION/SPI_COMMUNICATION.srcs/sources_1/new/spislave.v
@@ -12,19 +12,13 @@ module spislave (
 
     always @(posedge sclk or posedge ss) begin
         if (ss) begin
-           
             shift_reg_out <= transmit_data;
             shift_reg_in <= 8'd0;
             bit_count <= 4'd0;
         end else begin
-           
             miso <= shift_reg_out[0];
             shift_reg_out <= {1'b0, shift_reg_out[7:1]};
-
-           
             shift_reg_in <= {mosi, shift_reg_in[7:1]};
-
-           
             if (bit_count == 7) begin
                 received_data <= {mosi, shift_reg_in[7:1]};
                 bit_count <= 0;
